2a42c3cf505134a6613ea99fefff4250eba2ec0c
Philippe Brossier <brossierp@gmail.com>
2017-07-04 15:09:46 +0100

    CTPA-1340 Retry does work ok for ActionFeedback. For CaseNotifs, still to determine the unique business key on retries

--- actionsvc/src/main/java/uk/gov/ons/ctp/response/action/message/impl/CaseNotificationReceiverImpl.java
+++ actionsvc/src/main/java/uk/gov/ons/ctp/response/action/message/impl/CaseNotificationReceiverImpl.java
@@ -9,6 +9,7 @@ import uk.gov.ons.ctp.response.action.message.CaseNotificationReceiver;
 import uk.gov.ons.ctp.response.action.service.CaseNotificationService;
 import uk.gov.ons.ctp.response.casesvc.message.notification.CaseNotifications;
 
+import javax.validation.constraints.Null;
 import java.util.stream.Collectors;
 
 /**
@@ -21,13 +22,13 @@ public class CaseNotificationReceiverImpl implements CaseNotificationReceiver {
   @Autowired
   private CaseNotificationService caseNotificationService;
 
-  // TODO CTPA-1340
   @Override
   @ServiceActivator(inputChannel = "caseNotificationTransformed", adviceChain = "caseNotificationRetryAdvice")
   public void acceptNotification(CaseNotifications caseNotifications) throws CTPException {
     log.debug("Receiving case notifications for case ids {}", caseNotifications.getCaseNotifications().stream()
               .map(cn -> cn.getCaseId().toString())
               .collect(Collectors.joining(",")));
+//    throw new NullPointerException("zzz acceptNotification");
     caseNotificationService.acceptNotification(caseNotifications.getCaseNotifications());
   }
 }
