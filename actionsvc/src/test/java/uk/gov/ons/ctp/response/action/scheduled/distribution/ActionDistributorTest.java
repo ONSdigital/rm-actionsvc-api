1358f49d33ef92e636e7f2c8586b2270b83ab724
garethTurner <gareth.turner@ons.gov.uk>
2017-07-18 15:51:09 +0100

    CTPA-1366  improve actionDistributor test coverage

--- actionsvc/src/test/java/uk/gov/ons/ctp/response/action/scheduled/distribution/ActionDistributorTest.java
+++ actionsvc/src/test/java/uk/gov/ons/ctp/response/action/scheduled/distribution/ActionDistributorTest.java
@@ -29,7 +29,6 @@ import org.springframework.transaction.support.TransactionTemplate;
 import ma.glasnost.orika.MapperFacade;
 import uk.gov.ons.ctp.common.FixtureHelper;
 import uk.gov.ons.ctp.common.distributed.DistributedListManager;
-import uk.gov.ons.ctp.common.rest.RestClient;
 import uk.gov.ons.ctp.common.state.StateTransitionManager;
 import uk.gov.ons.ctp.response.action.config.ActionDistribution;
 import uk.gov.ons.ctp.response.action.config.AppConfig;
@@ -47,12 +46,9 @@ import uk.gov.ons.ctp.response.action.representation.ActionDTO;
 import uk.gov.ons.ctp.response.action.representation.ActionDTO.ActionState;
 import uk.gov.ons.ctp.response.action.service.CaseSvcClientService;
 import uk.gov.ons.ctp.response.action.service.impl.PartySvcClientServiceImpl;
-import uk.gov.ons.ctp.response.casesvc.representation.CaseDTO;
 import uk.gov.ons.ctp.response.casesvc.representation.CaseDetailsDTO;
 import uk.gov.ons.ctp.response.casesvc.representation.CaseEventDTO;
-import uk.gov.ons.ctp.response.casesvc.representation.CaseGroupDTO;
 import uk.gov.ons.ctp.response.casesvc.representation.CategoryDTO;
-import uk.gov.ons.ctp.response.collection.exercise.representation.CaseTypeDTO;
 import uk.gov.ons.ctp.response.party.representation.PartyDTO;
 
 /**
@@ -332,4 +328,201 @@ public class ActionDistributorTest {
     verify(actionInstructionPublisher, times(1)).sendActionInstructions(eq("HHSurvey"), anyListOf(ActionRequest.class),
         anyListOf(ActionCancel.class));
   }
+  
+  /**
+   * Test BlueSky scenario - two action types, four cases etc resulting in two
+   * calls to publish
+   * 
+   * @throws Exception oops
+   */
+  @Test
+  public void testDistributorForActionCancels() throws Exception {
+
+    List<ActionType> actionTypes = FixtureHelper.loadClassFixtures(ActionType[].class);
+
+    List<Action> actionsHHIC = FixtureHelper.loadClassFixtures(Action[].class, "CancelHouseholdInitial");
+    List<Action> actionsHHIACLOAD = FixtureHelper.loadClassFixtures(Action[].class, "CancelHouseholdUpload");
+
+    List<ActionPlan> actionPlans = FixtureHelper.loadClassFixtures(ActionPlan[].class);
+
+    List<CaseEventDTO> caseEventDTOsPost = FixtureHelper.loadClassFixtures(CaseEventDTO[].class, "post");
+
+    // wire up mock responses
+    Mockito.when(
+        actionSvcStateTransitionManager.transition(ActionState.CANCEL_SUBMITTED, ActionDTO.ActionEvent.CANCELLATION_DISTRIBUTED))
+        .thenReturn(ActionState.CANCEL_PENDING);
+
+    Mockito.when(actionTypeRepo.findAll()).thenReturn(actionTypes);
+    Mockito.when(actionPlanRepo.findOne(any(Integer.class))).thenReturn(actionPlans.get(0));
+    Mockito
+        .when(actionRepo.findByActionTypeNameAndStateInAndActionPKNotIn(eq("HouseholdInitialContact"),
+            anyListOf(ActionState.class), anyListOf(BigInteger.class), any(Pageable.class)))
+        .thenReturn(actionsHHIC);
+    Mockito.when(
+        actionRepo.findByActionTypeNameAndStateInAndActionPKNotIn(eq("HouseholdUploadIAC"),
+            anyListOf(ActionState.class), anyListOf(BigInteger.class), any(Pageable.class)))
+        .thenReturn(actionsHHIACLOAD);
+
+    Mockito.when(
+        caseSvcClientService.createNewCaseEvent(any(Action.class), eq(CategoryDTO.CategoryName.ACTION_CREATED)))
+        .thenReturn(caseEventDTOsPost.get(2));
+
+
+    // let it roll
+    actionDistributor.distribute();
+
+    // assert the right calls were made
+    verify(actionTypeRepo).findAll();
+    verify(actionRepo).findByActionTypeNameAndStateInAndActionPKNotIn(eq("HouseholdInitialContact"),
+        anyListOf(ActionState.class), anyListOf(BigInteger.class), any(Pageable.class));
+    verify(actionRepo).findByActionTypeNameAndStateInAndActionPKNotIn(eq("HouseholdUploadIAC"),
+        anyListOf(ActionState.class), anyListOf(BigInteger.class), any(Pageable.class));
+
+    verify(caseSvcClientService, times(4)).createNewCaseEvent(any(Action.class),
+        eq(CategoryDTO.CategoryName.ACTION_CANCELLATION_CREATED));
+
+    verify(actionInstructionPublisher, times(1)).sendActionInstructions(eq("Printer"), anyListOf(ActionRequest.class),
+        anyListOf(ActionCancel.class));
+    verify(actionInstructionPublisher, times(1)).sendActionInstructions(eq("HHSurvey"), anyListOf(ActionRequest.class),
+        anyListOf(ActionCancel.class));
+  }
+  
+  
+  /**
+   * Test BlueSky scenario - two action types, four cases etc resulting in two
+   * calls to publish
+   * 
+   * @throws Exception oops
+   */
+  @Test
+  public void testBlueSkyActionRequestSizeEqualDistributionMax() throws Exception {
+    
+    CaseSvc caseSvcConfig = new CaseSvc();
+    ActionDistribution actionDistributionConfig = new ActionDistribution();
+    actionDistributionConfig.setDelayMilliSeconds(I_HATE_CHECKSTYLE_TEN);
+    actionDistributionConfig.setRetrievalMax(I_HATE_CHECKSTYLE_TEN);
+    actionDistributionConfig.setDistributionMax(2);
+    actionDistributionConfig.setRetrySleepSeconds(I_HATE_CHECKSTYLE_TEN);
+
+    appConfig.setCaseSvc(caseSvcConfig);
+    appConfig.setActionDistribution(actionDistributionConfig);
+
+    MockitoAnnotations.initMocks(this);
+
+    List<ActionType> actionTypes = FixtureHelper.loadClassFixtures(ActionType[].class);
+
+    List<Action> actionsHHIC = FixtureHelper.loadClassFixtures(Action[].class, "HouseholdInitialContact");
+    List<Action> actionsHHIACLOAD = FixtureHelper.loadClassFixtures(Action[].class, "HouseholdUploadIAC");
+
+    List<CaseDetailsDTO> caseDTOs = FixtureHelper.loadClassFixtures(CaseDetailsDTO[].class);
+
+    List<ActionPlan> actionPlans = FixtureHelper.loadClassFixtures(ActionPlan[].class);
+
+    List<CaseEventDTO> caseEventDTOsPost = FixtureHelper.loadClassFixtures(CaseEventDTO[].class, "post");
+
+    List<PartyDTO> partyDTOs = FixtureHelper.loadClassFixtures(PartyDTO[].class);
+
+    // wire up mock responses
+    Mockito.when(
+        actionSvcStateTransitionManager.transition(ActionState.SUBMITTED, ActionDTO.ActionEvent.REQUEST_DISTRIBUTED))
+        .thenReturn(ActionState.PENDING);
+
+    Mockito.when(actionTypeRepo.findAll()).thenReturn(actionTypes);
+    Mockito.when(actionPlanRepo.findOne(any(Integer.class))).thenReturn(actionPlans.get(0));
+    Mockito
+        .when(actionRepo.findByActionTypeNameAndStateInAndActionPKNotIn(eq("HouseholdInitialContact"),
+            anyListOf(ActionState.class), anyListOf(BigInteger.class), any(Pageable.class)))
+        .thenReturn(actionsHHIC);
+    Mockito.when(
+        actionRepo.findByActionTypeNameAndStateInAndActionPKNotIn(eq("HouseholdUploadIAC"),
+            anyListOf(ActionState.class), anyListOf(BigInteger.class), any(Pageable.class)))
+        .thenReturn(actionsHHIACLOAD);
+
+    Mockito.when(
+        caseSvcClientService.getCaseWithIACandCaseEvents(eq(UUID.fromString("7fac359e-645b-487e-bb02-70536eae51d4"))))
+        .thenReturn(caseDTOs.get(0));
+    Mockito.when(
+        caseSvcClientService.getCaseWithIACandCaseEvents(eq(UUID.fromString("3382981d-3df0-464e-9c95-aea7aee80c81"))))
+        .thenReturn(caseDTOs.get(0));
+
+    Mockito.when(
+        caseSvcClientService.createNewCaseEvent(any(Action.class), eq(CategoryDTO.CategoryName.ACTION_CREATED)))
+        .thenReturn(caseEventDTOsPost.get(2));
+
+    Mockito.when(partySvcClientService.getParty("H", UUID.fromString("2e6add83-e43d-4f52-954f-4109be506c86")))
+        .thenReturn(partyDTOs.get(0));
+
+    // let it roll
+    actionDistributor.distribute();
+
+    // assert the right calls were made
+    verify(actionTypeRepo).findAll();
+    verify(actionRepo).findByActionTypeNameAndStateInAndActionPKNotIn(eq("HouseholdInitialContact"),
+        anyListOf(ActionState.class), anyListOf(BigInteger.class), any(Pageable.class));
+    verify(actionRepo).findByActionTypeNameAndStateInAndActionPKNotIn(eq("HouseholdUploadIAC"),
+        anyListOf(ActionState.class), anyListOf(BigInteger.class), any(Pageable.class));
+
+    verify(caseSvcClientService, times(2))
+        .getCaseWithIACandCaseEvents(eq(UUID.fromString("3382981d-3df0-464e-9c95-aea7aee80c81")));
+    verify(caseSvcClientService, times(2))
+        .getCaseWithIACandCaseEvents(eq(UUID.fromString("7fac359e-645b-487e-bb02-70536eae51d4")));
+
+    verify(caseSvcClientService, times(4)).createNewCaseEvent(any(Action.class),
+        eq(CategoryDTO.CategoryName.ACTION_CREATED));
+
+    verify(actionInstructionPublisher, times(1)).sendActionInstructions(eq("Printer"), anyListOf(ActionRequest.class),
+        anyListOf(ActionCancel.class));
+    verify(actionInstructionPublisher, times(1)).sendActionInstructions(eq("HHSurvey"), anyListOf(ActionRequest.class),
+        anyListOf(ActionCancel.class));
+  }
+  
+  
+  /**
+   * Test that when we fail at first hurdle to load ActionTypes we do not go on
+   * to call anything else In reality the wakeup mathod would then be called
+   * again after a sleep interval by spring but we cannot test that here
+   *
+   * @throws Exception oops
+   */
+  @Test
+  public void testActionsEmptyList() throws Exception {
+
+    List<ActionType> actionTypes = FixtureHelper.loadClassFixtures(ActionType[].class);
+    
+    Mockito.when(actionTypeRepo.findAll()).thenReturn(actionTypes);
+    // let it roll
+    actionDistributor.distribute();
+
+    // assert the right calls were made
+    verify(actionTypeRepo).findAll();
+    verify(actionRepo, times(1)).findByActionTypeNameAndStateInAndActionPKNotIn(
+        eq("HouseholdInitialContact"),
+        anyListOf(ActionState.class), anyListOf(BigInteger.class), any(Pageable.class));
+    verify(actionRepo, times(1)).findByActionTypeNameAndStateInAndActionPKNotIn(
+        eq("HouseholdUploadIAC"),
+        anyListOf(ActionState.class), anyListOf(BigInteger.class), any(Pageable.class));
+
+    verify(caseSvcClientService, times(0)).getCase(eq(
+        UUID.fromString("7fac359e-645b-487e-bb02-70536eae51d4")));
+    verify(caseSvcClientService, times(0)).getCase(eq(
+        UUID.fromString("7fac359e-645b-487e-bb02-70536eae51d4")));
+
+    verify(partySvcClientService, times(0)).getParty(eq("B"),
+        eq(UUID.fromString("7bc5d41b-0549-40b3-ba76-42f6d4cf3992")));
+
+    verify(caseSvcClientService, times(0)).getCaseEvents(eq(
+        UUID.fromString("7fac359e-645b-487e-bb02-70536eae51d4")));
+    verify(caseSvcClientService, times(0)).getCaseEvents(eq(
+        UUID.fromString("7fac359e-645b-487e-bb02-70536eae51d4")));
+
+    verify(caseSvcClientService, times(0)).createNewCaseEvent(any(Action.class),
+        eq(CategoryDTO.CategoryName.ACTION_CREATED));
+
+    verify(actionInstructionPublisher, times(0)).sendActionInstructions(eq("Printer"),
+        anyListOf(ActionRequest.class), anyListOf(ActionCancel.class));
+    verify(actionInstructionPublisher, times(0)).sendActionInstructions(eq("HHSurvey"),
+        anyListOf(ActionRequest.class), anyListOf(ActionCancel.class));
+  }
+  
+ 
 }
