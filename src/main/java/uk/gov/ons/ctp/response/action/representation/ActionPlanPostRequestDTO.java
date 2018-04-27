2f90201218eecdfaca876c0dd692e2ae50ce4460
Tejas Patel <tejas.patel@ons.gov.uk>
2018-04-27 13:50:00 +0100

    add action plan post request dto

---
+++ src/main/java/uk/gov/ons/ctp/response/action/representation/ActionPlanPostRequestDTO.java
@@ -0,0 +1,32 @@
+package uk.gov.ons.ctp.response.action.representation;
+
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * Domain model object for representation.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class ActionPlanPostRequestDTO {
+
+    @NotNull
+    @Size(max=100)
+    private String name;
+
+    @NotNull
+    @Size(max=250)
+    private String description;
+
+    @NotNull
+    @Size(max=20)
+    private String createdBy;
+
+}
