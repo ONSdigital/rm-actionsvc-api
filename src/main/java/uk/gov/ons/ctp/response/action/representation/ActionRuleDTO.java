300741d90035930e6845e54abcfd62fc3900c924
Tejas Patel <tejas.patel@ons.gov.uk>
2018-05-01 10:49:26 +0100

    remove white space

--- src/main/java/uk/gov/ons/ctp/response/action/representation/ActionRuleDTO.java
+++ src/main/java/uk/gov/ons/ctp/response/action/representation/ActionRuleDTO.java
@@ -16,14 +16,9 @@ import java.util.UUID;
 public class ActionRuleDTO {
 
   private UUID id;
-
   private String name;
-
   private String description;
-
   private Integer daysOffset;
-
   private Integer priority;
-
   private String actionTypeName;
 }
