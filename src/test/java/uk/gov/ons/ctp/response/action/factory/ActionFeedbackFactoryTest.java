291d82f142f68d776297aad13ddb453783f07614
Tom Oram <tom.oram@armakuni.com>
2018-07-06 13:55:42 +0100

    Add `ActionFeedbackFactory`
    So that `Situation` can be provided instead of a string when creating
    instances of `ActionFeedback`.
    
    It would be nice if we could get `jaxb` to generate the `ActionFeedback`
    class to accept `Situation`.

---
+++ src/test/java/uk/gov/ons/ctp/response/action/factory/ActionFeedbackFactoryTest.java
@@ -0,0 +1,28 @@
+package uk.gov.ons.ctp.response.action.factory;
+
+import org.junit.Test;
+import uk.gov.ons.ctp.response.action.message.feedback.ActionFeedback;
+import uk.gov.ons.ctp.response.action.message.feedback.Outcome;
+import uk.gov.ons.ctp.response.action.representation.Situation;
+import uk.gov.ons.ctp.response.action.representation.exception.InvalidSituationException;
+
+import static org.junit.Assert.assertEquals;
+
+public class ActionFeedbackFactoryTest {
+
+  public static final String ACTION_ID = "08073f47-1d8d-44cf-b035-31c398ec46bf";
+  public static final String SITUATION_STRING = "NotifyGateway";
+  public static final Outcome OUTCOME = Outcome.CANCELLATION_ACCEPTED;
+
+  @Test
+  public void
+  testItCreatesAnActionFeedbackInstance() throws InvalidSituationException {
+    final Situation situation = new Situation(SITUATION_STRING);
+
+    ActionFeedback actionFeedback = ActionFeedbackFactory.create(ACTION_ID, situation, OUTCOME);
+
+    assertEquals(ACTION_ID, actionFeedback.getActionId());
+    assertEquals(SITUATION_STRING, actionFeedback.getSituation());
+    assertEquals(Outcome.CANCELLATION_ACCEPTED, actionFeedback.getOutcome());
+  }
+}
